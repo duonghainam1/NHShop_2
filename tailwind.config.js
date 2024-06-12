c92bcfce0a9d694f62cd476c9690e313ea7b6978
duonghainam1 <namhai0301004@gmail.com>
2024-06-12 09:35:55 +0700

    1

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: { "Secondary": "#05422C", "primary": "#17AF26" }
+    },
+  },
+  plugins: [],
+}
